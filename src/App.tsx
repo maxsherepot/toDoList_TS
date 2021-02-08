4fe697bc4267e9e8960e0e9e8f4325c53222c326
maxsherepot <48758609+maxsherepot@users.noreply.github.com>
2021-02-08 17:56:55 +0200

    new task input added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import NewTaskInput from './components/NewTaskInput';
 
 
 const App: React.FC = () => {
 
   return (
     <div className="container">
+      <NewTaskInput />
     </div>
   );
 }
---
+++ src/components/NewTaskInput.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const NewTaskInput: React.FC = () => {
+
+    return (
+        <div className="mt-5 d-flex justify-content-between">
+            <form>
+                <div className="form-outline">
+                    <input type="text" id="form1Example2"
+                        className="form-control border" />
+                    <label className="form-label" htmlFor="form1Example2">Password</label>
+                </div>
+            </form>
+            <button className="btn special-color">Add task</button>
+
+        </div>
+    );
+}
+
+
+export default NewTaskInput;
